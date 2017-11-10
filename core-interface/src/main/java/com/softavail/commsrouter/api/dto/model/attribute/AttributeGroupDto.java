60735d4a7919db77fd132c70e57ac6b43b7de3bf
Ergyun Syuleyman <ergun@softavail.com>
2017-11-10 17:45:07 +0200

    added/updatred Apache license header in all projects source java and POM files

--- core-interface/src/main/java/com/softavail/commsrouter/api/dto/model/attribute/AttributeGroupDto.java
+++ core-interface/src/main/java/com/softavail/commsrouter/api/dto/model/attribute/AttributeGroupDto.java
@@ -1,6 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project AttributeGroupDto. To change
- * this template file, choose Tools | Templates and open the template in the editor.
+/* 
+ * Copyright 2017 SoftAvail Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.softavail.commsrouter.api.dto.model.attribute;
@@ -52,18 +63,18 @@ public class AttributeGroupDto extends HashMap<String, AttributeValueDto> {
     get(ArrayOfDoublesAttributeValueDto.class, name).add(value);
   }
 
-  public <T extends ArrayAttributeValueDto> T get(Class<T> c, String name) {
-    T t = c.cast(get(name));
-    if (t != null) {
-      return t;
+  public <T extends ArrayAttributeValueDto> T get(Class<T> ct, String name) {
+    T tv = ct.cast(get(name));
+    if (tv != null) {
+      return tv;
     }
     try {
-      t = c.newInstance();
+      tv = ct.newInstance();
     } catch (InstantiationException | IllegalAccessException ex) {
       throw new RuntimeException("Unexpected ArrayAttributeValueDto instantiation exception", ex);
     }
-    put(name, t);
-    return t;
+    put(name, tv);
+    return tv;
   }
 
 }
--- core/src/main/java/com/softavail/commsrouter/eval/HasFunction.java
+++ core/src/main/java/com/softavail/commsrouter/eval/HasFunction.java
@@ -1,6 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties. To change this
- * template file, choose Tools | Templates and open the template in the editor.
+/* 
+ * Copyright 2017 SoftAvail Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.softavail.commsrouter.eval;
@@ -20,7 +31,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author ergyunsyuleyman
+ * @author Ergyun Syuleyman
  * 
  *         This class is a function that executes within Evaluator. The function check for the first
  *         array parameter contains or not the item in the second parameter. See the
