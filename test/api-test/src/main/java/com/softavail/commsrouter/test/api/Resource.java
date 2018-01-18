4c0f735ae1dfa52a84cd30109400b179d16555fb
A.Popov <angelpopov@yahoo.com>
2018-01-18 15:50:58 +0200

    Tests: Use env variable AUT_HOST if nothing is specified on command line

--- test/api-test/src/main/java/com/softavail/commsrouter/test/api/Resource.java
+++ test/api-test/src/main/java/com/softavail/commsrouter/test/api/Resource.java
@@ -28,13 +28,14 @@ public class Resource {
   public Resource(HashMap<CommsRouterResource, String> state) {
     RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     java.util.Map<String, String> env = System.getenv();
-    String host = env.get("AUT_HOST");
-    if (host != null){
-      System.setProperty("autHost", host);
-    }
 
     if (System.getProperty("autHost") == null) {
-      RestAssured.baseURI = "http://localhost:8080";
+      String host = env.get("AUT_HOST");
+      if (host != null){
+        RestAssured.baseURI = host;
+      } else {
+        RestAssured.baseURI = "http://localhost:8080";
+      }
     } else {
       // you can specify it using -DautHost=http://localhost:8080
       RestAssured.baseURI = System.getProperty("autHost");
