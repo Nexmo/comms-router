278f55b09605a76118dc0696fd35d8095c7e17c4
Vladislav Todorov <vladislav@softavail.com>
2018-03-23 15:28:32 +0200

    Do not visit node with RsqlDummyValidator.

--- web/src/main/java/com/softavail/commsrouter/webservice/ApplicationContext.java
+++ web/src/main/java/com/softavail/commsrouter/webservice/ApplicationContext.java
@@ -23,7 +23,7 @@ import com.softavail.commsrouter.app.TaskDispatcher;
 import com.softavail.commsrouter.domain.dto.mappers.EntityMappers;
 import com.softavail.commsrouter.eval.CommsRouterEvaluatorFactory;
 import com.softavail.commsrouter.eval.RsqlValidator;
-import com.softavail.commsrouter.eval.RsqlSkillsValidator;
+import com.softavail.commsrouter.eval.RsqlSkillValidator;
 import com.softavail.commsrouter.eval.RsqlDummyValidator;
 import com.softavail.commsrouter.jpa.JpaDbFacade;
 import com.softavail.commsrouter.webservice.config.ConfigurationImpl;
@@ -120,7 +120,7 @@ public class ApplicationContext {
 
   private RsqlValidator createRsqlValidator() {
     if (configuration.getClientEnableSkillValidation()) {
-      return new RsqlSkillsValidator(coreContext.svc.skill);
+      return new RsqlSkillValidator(coreContext.svc.skill);
     } else {
       return new RsqlDummyValidator();
     }
