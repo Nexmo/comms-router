ff0ce1fd535401f802a6d2f744606f047fd395c8
mapuo <mario@softavail.com>
2018-05-31 16:39:20 +0300

    make it possible to query objects on their Attribute properties   (Agent.capabilities, Task.requirements, Task.userContext...)

--- core/src/main/java/com/softavail/commsrouter/api/service/FilterHelper.java
+++ core/src/main/java/com/softavail/commsrouter/api/service/FilterHelper.java
@@ -17,13 +17,26 @@
 
 package com.softavail.commsrouter.api.service;
 
+import com.github.tennaito.rsql.builder.BuilderTools;
 import com.github.tennaito.rsql.jpa.JpaPredicateVisitor;
+import com.github.tennaito.rsql.jpa.PredicateBuilderStrategy;
 import cz.jirutka.rsql.parser.RSQLParser;
+import cz.jirutka.rsql.parser.ast.ComparisonNode;
+import cz.jirutka.rsql.parser.ast.ComparisonOperator;
 import cz.jirutka.rsql.parser.ast.Node;
+import cz.jirutka.rsql.parser.ast.RSQLOperators;
 import cz.jirutka.rsql.parser.ast.RSQLVisitor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.From;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -32,20 +45,101 @@ import javax.persistence.criteria.Root;
  */
 public class FilterHelper {
 
+  private static final Logger LOGGER = LogManager.getLogger(FilterHelper.class);
+
+  private static final String ATTR_OPERATOR = "=attr=";
+
   public static Optional<Predicate> filterPredicate(String query, Root root, EntityManager em) {
     if (query != null && !query.isEmpty()) {
       // Create the JPA Visitors
-      RSQLVisitor<Predicate, EntityManager> visitor = new JpaPredicateVisitor<>().defineRoot(root);
+      AttributeOpStrategy predicateStrategy = new AttributeOpStrategy();
+      FilterPredicateVisitor<?> jpaVisitor = new FilterPredicateVisitor<>();
+      jpaVisitor.getBuilderTools().setPredicateBuilder(predicateStrategy);
+      RSQLVisitor<Predicate, EntityManager> visitor = jpaVisitor.defineRoot(root);
+
+      Set<ComparisonOperator> operators = RSQLOperators.defaultOperators();
+      operators.add(new ComparisonOperator(ATTR_OPERATOR, true));
 
       // Parse a RSQL into a Node
-      Node rootNode = new RSQLParser().parse(query);
+      Node rootNode = new RSQLParser(operators).parse(query);
 
       // Visit the node to retrieve CriteriaQuery
       Predicate predicate = rootNode.accept(visitor, em);
-      return Optional.of(predicate);
+      return Optional.ofNullable(predicate);
     }
 
     return Optional.empty();
+  }
+
+  public static class FilterPredicateVisitor<T> extends JpaPredicateVisitor<T> {
+
+    private From root;
+
+    @Override
+    public FilterPredicateVisitor<T> defineRoot(From root) {
+      this.root = root;
+      super.defineRoot(root);
+      return this;
+    }
+
+    @Override
+    public Predicate visit(ComparisonNode node, EntityManager entityManager) {
+      try {
+        return getBuilderTools().getPredicateBuilder().createPredicate(
+            node, root, entityClass, entityManager, getBuilderTools());
+      } catch (IllegalArgumentException exp) {
+        LOGGER.error("Error!", exp);
+        return super.visit(node, entityManager);
+      }
+    }
+  }
+
+  public static class AttributeOpStrategy implements PredicateBuilderStrategy {
+
+    private static final Logger LOGGER = LogManager.getLogger(AttributeOpStrategy.class);
+
+    private static final Pattern SELECTOR_PATTERN = Pattern.compile(
+        "(?<property>[^\\.]+)\\.(?<key>\\w+)");
+
+    @Override
+    public <T> Predicate createPredicate(
+        Node node,
+        From root,
+        Class<T> entity,
+        EntityManager manager,
+        BuilderTools tools)
+        throws IllegalArgumentException {
+
+      LOGGER.debug("root: {}", root);
+      LOGGER.debug("root: {}", root.getJavaType());
+
+      ComparisonNode comp = (ComparisonNode) node;
+
+      LOGGER.debug("operator: {}", comp.getOperator());
+      LOGGER.debug("selector: {}", comp.getSelector());
+      LOGGER.debug("arguments: {}", comp.getArguments());
+
+      Matcher matcher = SELECTOR_PATTERN.matcher(comp.getSelector());
+      if (matcher.find()) {
+        String property = matcher.group("property");
+        String key = matcher.group("key");
+        LOGGER.debug("property: {}, key: {}", property, key);
+
+        CriteriaBuilder cb = manager.getCriteriaBuilder();
+
+        Join attributeGroup = root.join(property);
+        Join attribute = attributeGroup.join("attributes");
+
+        Predicate where = cb.and(
+            cb.equal(attribute.get("name"), key),
+            attribute.get("stringValue").in(comp.getArguments()));
+
+        return cb.and(where);
+      }
+
+      return null;
+    }
+
   }
 
 }
